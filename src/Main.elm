412204cc621d270d96f664db07d0013fc87f165c
richyliu <richy.liu.2002@gmail.com>
2019-03-01 17:59:27 -0800

    initial

---
+++ src/Main.elm
@@ -0,0 +1,13 @@
+module Main exposing (main)
+
+import Html exposing (h1, text)
+
+
+
+{- Changes to this Elm file are only detected by Parcel once, after which
+   changes get ignored
+-}
+
+
+main =
+    h1 [] [ text "Hello, world" ]
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * Commenting out this line and then uncommenting this line forces Parcel to
+ * compile the Elm file
+ */
+import { Elm } from './Main.elm';
+
+Elm.Main.init({
+  node: document.querySelector('main')
+});
